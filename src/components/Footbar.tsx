34a0616bb58bece8fc30de64ca172f399a951e5e
SimplyRohit <rohitjaatjaat073@gmail.com>
2024-10-17 15:12:03 +0530

    "new" : frontend

---
+++ src/components/Footbar.tsx
@@ -0,0 +1,13 @@
+export default function Footbar() {
+  return (
+    <div className="absolute bottom-0  left-0 right-0  flex justify-center gap-2 text-[0.8rem] text-opacity-50 text-gray-800 items-center">
+      <p className="">team </p>
+      <p>| </p>
+      <p>discord </p>
+      <p>| </p>
+      <p>Twitter </p>
+      <p>| </p>
+      <p>Buy me coffee</p>
+    </div>
+  );
+}
---
+++ src/components/Sound.tsx
@@ -0,0 +1,57 @@
+"use client";
+import {
+  VolumeOff,
+  Volume2,
+  Headphones,
+  HeadphoneOff,
+  LogOut,
+} from "lucide-react";
+import { useState } from "react";
+export default function Sound() {
+  const [isSound, setSound] = useState(true);
+  const [isMusic, setMusic] = useState(true);
+  return (
+    <div className="z-10 absolute right-0 md:top-0 top-28 m-5">
+      <div className=" flex items-center justify-end">
+        <h1 className="text-xl md:block hidden opacity-40">Exit</h1>
+        <LogOut className="w-6 mb-2 ml-2 text-[#8B5E5E]" />
+      </div>
+      <div className=" flex items-center justify-end">
+        <h1 className="text-xl md:block hidden opacity-40 ">Sound</h1>
+        {isSound ? (
+          <Volume2
+            onClick={() => {
+              setSound(false);
+            }}
+            className="w-6 mb-2 ml-2 text-[#8B5E5E]"
+          />
+        ) : (
+          <VolumeOff
+            onClick={() => {
+              setSound(true);
+            }}
+            className="w-6 mb-2 ml-2 text-[#8B5E5E]"
+          />
+        )}
+      </div>
+      <div className=" flex items-center justify-end">
+        <h1 className="text-xl md:block hidden opacity-40 ">Music</h1>
+        {isMusic ? (
+          <Headphones
+            onClick={() => {
+              setMusic(false);
+            }}
+            className="w-6 mb-2 ml-2 text-[#8B5E5E]"
+          />
+        ) : (
+          <HeadphoneOff
+            onClick={() => {
+              setMusic(true);
+            }}
+            className="w-6 mb-2 ml-2 text-[#8B5E5E]"
+          />
+        )}
+      </div>
+    </div>
+  );
+}
